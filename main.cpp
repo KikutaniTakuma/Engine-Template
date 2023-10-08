5521cfeb319d30106d62aa8badc9303eed72b1dd
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-10-08 21:39:49 +0900

    デフォルトでキューブを表示するように変更

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	Camera camera;
 	camera.pos.z -= 10.0f;
 	Model model;
-	model.LoadObj("./Resources/Watame/Watame.obj");
+	model.LoadObj("./Resources/Cube.obj");
 
 	/// 
 	/// メインループ
