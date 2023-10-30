c5f760d888d91fd8556086da397a0002a01187f5
kikutani Takuma <k022g0126@g.neec.ac.jp>
2023-10-31 00:45:30 +0900

    エッジ抽出シェーダーを追加

--- Resources/Shaders/PostShader/PostEdge.PS .hlsl
+++ Resources/Shaders/PostShader/PostEdge.PS .hlsl
@@ -6,7 +6,7 @@ float4 main(Output input) : SV_TARGET{
 
     float dx = 1.0f / w;
     float dy = 1.0f / h;
-    float4 ret = float4(0.0f,0.0f,0.0f,0.0f);
+    float4 ret = float4(0.0f,0.0f,0.0f,1.0f);
 
     // 一つ上
     //ret += tex.Sample(smp, input.uv + float2(-1.0f * dx, 1.0f * dy));
@@ -24,8 +24,9 @@ float4 main(Output input) : SV_TARGET{
 
     float Y = dot(ret.xyz, float3(0.299f, 0.587f, 0.114f));
 
-    Y = pow(1.0f-Y, 20.0f);
-    Y = Y - 1.0f;
+    Y = pow(1.0f-Y,10.0f);
+
+    ret.a = tex.Sample(smp, input.uv).a;
 
     return float4(Y,Y,Y,ret.a);
 }
